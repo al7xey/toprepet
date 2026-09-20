07643f342f30150746bfcc420162026727183b4f
al7xey <al7xey@users.noreply.github.com>
2026-09-20 14:15:19 +0300

    Prevent stale assets after deployment

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,6 @@ export default defineConfig({
     cssCodeSplit: true,
     rolldownOptions: {
       output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name][extname]',
         codeSplitting: {
           groups: [
             {
